c9c7cf366c2b3106bc295dec9de86fd67d8bb1bf
Mishyn Aliaksei <trouvere777@mail.ru>
2020-05-04 21:21:40 +0300

    Aded working with server fetch and changed posts

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,19 +1,56 @@
 import React, { Component } from "react";
-import Post from "./post";
+import InstaService from "../services/instaservice";
+import User from "./user";
+import ErrorMessage from "./error";
 
 export default class Posts extends Component {
+  InstaService = new InstaService();
+  state = {
+    posts: [],
+    error: false,
+  };
+  componentDidMount() {
+    this.updatePosts();
+  }
+  updatePosts() {
+    this.InstaService.getAllPosts()
+      .then(this.onPostsLoaded)
+      .catch(this.onError);
+  }
+
+  onPostsLoaded = (posts) => {
+    this.setState({
+      posts,
+      error: false,
+    });
+    console.log(this.state);
+  };
+  onError = (err) => {
+    this.setState({
+      error: true,
+    });
+  };
+  renderItems(arr) {
+    return arr.map((item) => {
+      const { name, altname, photo, src, alt, descr, id } = item;
+      return (
+        <div key={id} className="post">
+          <User src={photo} alt={altname} name={name} min />
+          <img src={src} alt={alt} />
+          <div className="post__name">{name}</div>
+          <div className="post__descr">{descr}</div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return (
-      <div className="left">
-        <Post
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGN4c2ZYSeBsZMG6ed2xiHOnZ9Wb2zqaXid4jzXB6yJ4U7p8ki&usqp=CAU"
-          alt="insta"
-        />
-        <Post
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRjAEGyoocFZDUOgaZ22gc61WUKHsLv6igL3EGo4lYuHPm0XXnE&usqp=CAU"
-          alt="insta2"
-        />
-      </div>
-    );
+    const { error, posts } = this.state;
+
+    if (error) {
+      return <ErrorMessage />;
+    }
+    const items = this.renderItems(posts);
+    return <div className="left">{items}</div>;
   }
 }
